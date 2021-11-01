7bdf02a4130060daf28ec11ccff1785aedb18834
grassele <91402260+grassele@users.noreply.github.com>
2021-10-31 22:43:34 -0700

    Add files via upload
    bool library not working

--- cploration/main.c
+++ cploration/main.c
@@ -1,47 +1,29 @@
-#include <stdio.h>
-#include <string.h>
+#include "parser.h" // use quotes instead of <> for custom file instead of built-in library
 
-#define MAX_LIMIT 100
+int main(int argc, const char *argv[]) {
+    
+    // check that user entered exactly one argument
+    if (argc != 2) {
+        printf("Usage: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE); 
+    }
 
-int main() {
+    // open the argument passed as a file and save it as a file pointer
+    FILE *file = fopen( argv[1], "r" );
+    
+    // check if your file pointer is NULL, and if so, print error and exit w a failure
+    if (file == NULL) {
+        perror("Unable to open file!");
+        exit(EXIT_FAILURE);
+     }
+    
+    // call function parse and pass it the file pointer
+    parse(file);
 
-// declare a string with value
-char hello[] = "Hello";
-// declare an empty string
-char name[MAX_LIMIT];
+    // close file pointer
+    fclose(file);
 
-// prompt user
-printf("What is your name? ");
+   
 
-// read in a string from user and save in variable
-// [^\n] means to discard the newline character
-scanf("%[^\n]s", name);
 
-// print out "hello <name>"
-printf("%s %s!\n\n", hello, name);
-
-/** Exercise 1 **/
-char welcome[MAX_LIMIT];
-strcpy(welcome, hello);
-strcat(welcome, " ");
-strcat(welcome, name);
-printf(welcome, "\n");
-
-/** Exercise 2 **/ 
-int name_length = strlen(name);
-printf("Your name is %i letters long\n", name_length);
-
-/** Exercise 3 **/
-char prof[8] = {'P','a','t','r','i','c','k','\0'};
-if(strcmp(name, prof) < 0) {
-        printf("%s is before %s\n", name, prof);
-}
-else if(strcmp(name,prof) > 0){
-        printf("%s is after %s\n", name, prof);
- }
- else {
-        printf("%s is %s\n",name, prof);
-}
-
-return 0;
 }
