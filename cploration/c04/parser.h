bf8e42d33f89349d92574be93eb24f57148612aa
grassele <91402260+grassele@users.noreply.github.com>
2021-11-01 20:27:54 -0700

    Add files via upload
    update after friday office hours

--- cploration/c04/parser.h
+++ cploration/c04/parser.h
@@ -1,9 +1,9 @@
-#include <cstio.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
 #define MAX_LINE_LENGTH 200
 
-char *strip(char *s);
+char *strip(char *s); // char* strip(char*;)
 void parse(FILE * file);
