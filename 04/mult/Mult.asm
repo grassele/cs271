72d42552efa70f4fe5ec5870f650f55ea13cff79
grassele <91402260+grassele@users.noreply.github.com>
2021-11-07 22:48:36 -0800

    comparison failure line 2 mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,44 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Add R0 to R2 R1 times
+
+// test 4*5
+    @4
+    D=A
+    @R0
+    M=D
+    @5
+    D=A
+    @R1
+    M=D
+
+// initialize R2 to make sure we start from 0
+    @0
+    D=A
+    @R2
+    M=D
+
+// add R0 to R2
+(ADD_LOOP)
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @EXIT_CONDITION_LOOP
+    0;JMP
+
+// decrement R1 until it equals 0 (program has added R0 to R2 R1 times), then end the program
+(EXIT_CONDITION_LOOP)
+    @R1
+    M=M-1
+    @ADD_LOOP
+    M;JGT
+    @END
+    M;JEQ
+
+(END)
+    @END
+    0;JMP
+
+
