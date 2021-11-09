e9f864542d127118cf3fba1fe58fbdbef59c0a0b
grassele <91402260+grassele@users.noreply.github.com>
2021-11-09 11:21:49 -0800

    Remove excess files

--- cploration/c05/parser.h
+++ cploration/c05/parser.h
@@ -2,8 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define MAX_LINE_LENGTH 200
 
 char *strip(char *s); // char* strip(char*;)
 void parse(FILE * file);
+
+bool is_Atype(const char *);
+bool is_label(const char *);
+bool is_Ctype(const char *);
