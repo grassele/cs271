1ca4dbffc00277c2598f3d84fb5f90241558996b
grassele <91402260+grassele@users.noreply.github.com>
2021-12-06 17:25:02 -0800

    copied files from c09

---
+++ cploration/c10/error.h
@@ -0,0 +1,17 @@
+#ifndef __ERROR_H__
+#define __ERROR_H__
+
+#include <stdarg.h>
+
+enum exitcode {
+    EXIT_INCORRECT_ARGUMENTS = 1,   // means that the given file does not exist
+    EXIT_CANNOT_OPEN_FILE = 2,      // means file cannot be opened
+    EXIT_TOO_MANY_INSTRUCTIONS = 3,     // max num instructions is exceeded
+    EXIT_INVALID_LABEL = 4,
+    EXIT_SYMBOL_ALREADY_EXISTS = 5,
+    EXIT_INVALID_A_INSTR = 6
+};
+
+void exit_program(enum exitcode code, ...); // dot-dot-dot means it supports a variable-length argument list 
+
+#endif
